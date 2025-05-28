67b58913e59c13d4b508fea7f94177083bdecfa5
Jesse White <37096035+Jesse-ww@users.noreply.github.com>
2025-05-28 16:13:46 +0000

    Improved navigation and updated pages

---
+++ js/nav-menu.js
@@ -0,0 +1,57 @@
+// find the page name
+let splitPath = location.pathname.split("/");
+let pageName = splitPath[splitPath.length-1];
+let navItemId = "";
+
+// Set the nav item Id based on the page
+switch (pageName) {
+    case "index.html":
+        navItemId = "home";
+        break;
+    case "flipout-page.html":
+        navItemId = "fo";
+        break;
+    case "fo-pp.html":
+        navItemId = "fo";
+        break;
+    case "flipout-2-page.html":
+        navItemId = "fo2";
+        break;
+    case "fo2-pp.html":
+        navItemId = "fo2";
+        break;
+    case "about.html":
+        navItemId = "about";
+        break;
+}
+
+// Insert CSS reference
+let navCss = "<link rel='stylesheet' href='/css/nav-menu.css'>";
+let head = document.querySelector("head");
+head.innerHTML += navCss;
+
+// Insert nav menu
+let navMenu = `
+        <!-- Start Menu -->
+        <div id="test" class="menu-container flex-row flex-center">
+            <div class="nav-menu flex-row">
+                <a id="home" class="menu-item" href="/index.html">Home</a>
+                <a id="fo" class="menu-item" href="/flipout/flipout-page.html">Flipout</a>
+                <a id="fo2" class="menu-item" href="/flipout-2/flipout-2-page.html">Flipout 2</a>
+                <a id="about" class="menu-item" href="/about.html">About</a>
+            </div>
+        </div>
+        <!-- End Menu -->`;
+const body = document.querySelector("body");
+body.innerHTML = navMenu.concat(body.innerHTML);
+
+// Get all menu items and convert them to an Array
+const menu = document.querySelector(".menu-container");
+let menuItems = menu.querySelectorAll(".nav-menu .menu-item");
+menuItems = new Array(...menuItems);
+
+// Get the element that contains the class
+let item = menuItems.filter( (item) => item.id === navItemId )[0];
+
+// Set the "active" class on the element
+item.classList.add("active-menu-item");
